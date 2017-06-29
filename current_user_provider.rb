971ff444ead528f2fbb1d813a34fd59113a65fdf
Małgorzata Adamczyk <malgorzata.adamczyk@daftcode.pl>
2017-06-29 13:05:42 +0200

    set another cookie

--- current_user_provider.rb
+++ current_user_provider.rb
@@ -1,9 +1,10 @@
 class ExCurrentUserProvider < Auth::DefaultCurrentUserProvider
-  TOKEN_COOKIE ||= "_t".freeze
 
   def log_on_user(user, session, cookies)
     super
 
-    cookies.permanent[TOKEN_COOKIE] = { value: @user_token.unhashed_auth_token, httponly: true, domain: '.indoorway.com' }
+    data = { value: @user_token.unhashed_auth_token, httponly: true, domain: '.indoorway.com' }
+    cookies.permanent['_t'] = data
+    cookies.permanent['_forum_session'] = data
   end
 end
