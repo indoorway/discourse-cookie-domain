ab03b5a3f878c1cfca6fb771b3cb1ea69c3bc16f
Małgorzata Adamczyk <malgorzata.adamczyk@daftcode.pl>
2017-07-01 17:25:18 +0200

    wpi

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,5 @@
 Discourse::Application.config.session_store(
   :discourse_cookie_store,
   key: '_forum_session',
-  domain: '.indoorway.com',
-  path: (Rails.application.config.relative_url_root.nil?) ? '/' : Rails.application.config.relative_url_root
+  domain: '.indoorway.com'
 )
