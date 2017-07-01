c67bde55ffda47cd498e0adb0ac07398fc20a5b4
Małgorzata Adamczyk <malgorzata.adamczyk@daftcode.pl>
2017-07-01 14:48:38 +0200

    modify session_store

--- plugin.rb
+++ plugin.rb
@@ -6,3 +6,12 @@
 load File.expand_path("../current_user_provider.rb", __FILE__)
 
 Discourse.current_user_provider = ExCurrentUserProvider
+
+after_initialize do
+  Discourse::Application.config.session_store(
+    :discourse_cookie_store,
+    key: '_forum_session',
+    domain: '.indoorway.com',
+    path: (Rails.application.config.relative_url_root.nil?) ? '/' : Rails.application.config.relative_url_root
+  )
+end
